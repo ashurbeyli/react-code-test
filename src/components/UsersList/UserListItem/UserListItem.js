d5028d1bfdbe1e84cd799af91217a3c627f1d117
ashurbeyli <c.ashurbeyli@gmail.com>
2020-03-13 17:12:23 +0100

    feat: infinity scroll implemented

--- src/components/UsersList/UserListItem/UserListItem.js
+++ src/components/UsersList/UserListItem/UserListItem.js
@@ -4,11 +4,15 @@ import "./UserListItem.css";
 
 const getUserFullName = (firstName, lastName) => `${firstName} ${lastName}`;
 
-const UserListItem = ({ avatar, first_name, last_name }) => (
-  <li className="users-list__item">
-    <img className="users-list__item__img" src={avatar} />
-    <strong>{getUserFullName(first_name, last_name)}</strong>
-  </li>
-);
+const UserListItem = ({ avatar, first_name, last_name }) => {
+  const userFullName = getUserFullName(first_name, last_name);
+
+  return (
+    <li className="users-list__item">
+      <img alt={userFullName} className="users-list__item__img" src={avatar} />
+      <strong>{userFullName}</strong>
+    </li>
+  );
+};
 
 export default UserListItem;
